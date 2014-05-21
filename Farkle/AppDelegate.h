0a2f8918313bd14921d90ddac784f4957f2f1a4a
Kristen Fisher <kristen.fisher@d214.org>
2014-05-21 13:39:28 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Kristen L. Fisher on 5/21/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Farkle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Kristen L. Fisher on 5/21/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
